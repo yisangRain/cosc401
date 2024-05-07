0b2dfe8715150e503e89044f286a31686cf2c4f4
yisangSummer <yisang.summer@gmail.com>
2024-05-08 11:10:05 +1200

    wip

--- quiz4/log_reg_w_kernel.py
+++ quiz4/log_reg_w_kernel.py
@@ -26,49 +26,45 @@ def sigmoid(z):
 
 def logistic_regression_with_kernel(X, y, k, alpha, iterations):
     theta = np.zeros(X.shape[1])
-    bias = 0
 
-    for epoch in range(iterations):
+    for _ in range(iterations):
+        kernel = np.zeros(X.shape[1])
         for i in range(X.shape[0]):
             x = X[i]
             z = y[i]
-
-            prediction = sigmoid(k(theta, x) + bias)
+         
+            s_z = z * k(theta, x)
+            kernel = sigmoid(s_z) 
 
             #compute gradients
-            gradient_theta = x * (z - prediction)
+            theta += alpha * kernel
 
-            gradient_bias = z - prediction
-
-            theta = theta + alpha * gradient_theta
-            bias = bias + alpha * gradient_bias
-
-    def model(x, theta=theta, bias=bias, k=k):
-        return sigmoid(np.dot(theta, x.T) + bias)
+    def model(x, theta=theta):
+        return sigmoid(np.dot(theta, x))
     return model
 
 
 
 # The underlining function is y = 2x - 1, and examples are positive if y >= 0 
 # With m >= 1 we should be able to converge to the exact function
-training_examples = np.array([
-    (0.99, 1),
-    (0.58, 1),
-    (0.01, 0),
-    (0.33, 0),
-    (0.09, 0)
-])
+# training_examples = np.array([
+#     (0.99, 1),
+#     (0.58, 1),
+#     (0.01, 0),
+#     (0.33, 0),
+#     (0.09, 0)
+# ])
 
-X = training_examples[:,[0]]
-y = training_examples[:,1]
+# X = training_examples[:,[0]]
+# y = training_examples[:,1]
 
-h = logistic_regression_with_kernel(X, y, monomial_kernel(1), 0.1, 500)
+# h = logistic_regression_with_kernel(X, y, monomial_kernel(1), 0.1, 500)
 
-inputs = np.array([0.88, 0.15, 0.82, 0.07, 0.52])
+# inputs = np.array([0.88, 0.15, 0.82, 0.07, 0.52])
 
-print(f"{'x' : ^5}{'label' : >5}{'true': >5}")
-for x in inputs:
-    print(f"{x : <5}{int(h(x)) : ^5}{int(2 * x - 1 >= 0) : ^5}")
+# print(f"{'x' : ^5}{'label' : >5}{'true': >5}")
+# for x in inputs:
+#     print(f"{x : <5}{int(h(x)) : ^5}{int(2 * x - 1 >= 0) : ^5}")
 #   x  label true
 # 0.88   1    1
 # 0.15   0    0
@@ -76,15 +72,14 @@ for x in inputs:
 # 0.07   0    0
 # 0.52   1    1
 
-print(h(X))
 np.random.seed(0xc05c401)
-# Some random samples
+# # Some random samples
 X = 3 * np.random.random((100, 2)) - 1.5
 
-# Examples with be + if they live in the unit circle and - otherwise
+# # Examples with be + if they live in the unit circle and - otherwise
 y = np.array([x[0]**2 + x[1]**2 < 1 for x in X]) 
 
-# Quadratic monomials should find the separating plane in question 3
+# # Quadratic monomials should find the separating plane in question 3
 h = logistic_regression_with_kernel(X, y, monomial_kernel(2), 0.01, 750)
 
 np.random.seed(0xbee5)
