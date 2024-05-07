e503a39eba95c40c2e1b6f5ff8a7b47956630cfa
yisangSummer <yisang.summer@gmail.com>
2024-05-07 14:28:56 +1200

    monomial v2

--- quiz4/monomial.py
+++ quiz4/monomial.py
@@ -4,12 +4,16 @@ import math
 
 def monomial_kernel(d):
     
-    phi_list = [lambda x: 1]
+    def k(x, y, dimension = d):
+        m = len(x)
+    
+        phi_list = [lambda x: 1]
 
-    for i in range(1, d+1):
-        combi = set(itertools.combinations_with_replacement(range(d), i))
-        combi = combi.union(set(itertools.permutations(range(d), i)))
-        
+        combi = set()
+
+        for i in range(1, dimension+1):
+            combi = combi.union(set(itertools.product(range(m), repeat=i)))
+            
         for j in list(combi):
             def temp_function(z, combination = j):
                 subtotal = 1
@@ -19,10 +23,9 @@ def monomial_kernel(d):
             phi_list.append(temp_function)
 
 
-    def k(x, y, phi=phi_list):
-        phi_x = np.array([p(x) for p in phi])
-        phi_y = np.array([p(y) for p in phi])
- 
+        phi_x = np.array([p(x) for p in phi_list])
+        phi_y = np.array([p(y) for p in phi_list])
+
         return np.dot(phi_x, phi_y)
 
     return k 
@@ -56,3 +59,11 @@ def phi(z): # Probably a nicer way to do this...
 x = np.array([0.15, -0.6, 7.1])
 y = np.array([-3.1, 0.01, -0.24])
 print(np.isclose(k(x, y), np.dot(phi(x), phi(y))))
+
+# Will timeout if feature map is computed
+d = 100
+k = monomial_kernel(d)
+x = 0.1 * np.ones(100)
+y = -0.02 * np.ones(100)
+print(np.isclose(k(x, y), 5 / 6))
+# True
