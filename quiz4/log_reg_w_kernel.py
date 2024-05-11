65ec20a8bfed66c244e082c215e87cbf503f6983
yisangSummer <yisang.summer@gmail.com>
2024-05-11 22:12:00 +1200

    ????

--- quiz4/log_reg_w_kernel.py
+++ quiz4/log_reg_w_kernel.py
@@ -1,5 +1,7 @@
 import numpy as np
-import math
+import warnings
+
+warnings.filterwarnings('ignore')
 
 def monomial_kernel(d):
     def k(x, y, d=d):
@@ -23,8 +25,8 @@ def rbf_kernel(sigma):
     return k
 
 def sigmoid(z):
-    # print(z)
-    return 1 / (1 + math.exp(-z)) 
+    return 1 / (1 + np.exp(-z)) 
+
 
 def logistic_regression_with_kernel(X, y, k, alpha, iterations):
 
@@ -52,10 +54,8 @@ def logistic_regression_with_kernel(X, y, k, alpha, iterations):
             bias += (alpha * (t - (sigmoid_value))) 
 
     def model(x, beta=beta, bias=bias, k=k, ref=X):
-        z = sum([k(ref[i], x) * beta[i] for i in range(ref.shape[0])]) + bias 
-        sig = sigmoid(z)
-        # print(sig)
-        return round(sig)
+        z = np.sum([k(ref[i], x) * beta[i] for i in range(ref.shape[0])]) + bias 
+        return round(sigmoid(z))
     return model
 
 
@@ -139,7 +139,7 @@ def test3():
     # The underlining function is y = x^3 - x^2 - x/4 + 1/4, and examples are positive if y >= 0
     # With m >= 3 we should be able to converge to the exact function
 
-    h = logistic_regression_with_kernel(X, y, monomial_kernel(3), 0.1, 500)
+    h = logistic_regression_with_kernel(X, y, monomial_kernel(3), 0.1, 900)
 
     inputs = np.array([0.34, -0.72, 0.84, -0.35, 0.3])
 
@@ -153,6 +153,58 @@ def test3():
     # -0.35  1    1↩
     # 0.3    1    1
 
+def test4():
+    	
+    f = lambda x, y, z, w:  int(x*y*z - y**2*z*w/4 + x**4*w**3/8- y*w/2 >= 0)
+
+    training_examples = [
+        ([0.254, 0.782, 0.254, 0.569], 0),
+        ([0.237, 0.026, 0.237, 0.638], 0),
+        ([0.814, 0.18, 0.814, 0.707], 1),
+        ([0.855, 0.117, 0.855, 0.669], 1),
+        ([0.776, 0.643, 0.776, 0.628], 1),
+        ([0.701, 0.71, 0.701, 0.982], 0),
+        ([0.443, 0.039, 0.443, 0.356], 1),
+        ([0.278, 0.105, 0.278, 0.158], 0),
+        ([0.394, 0.203, 0.394, 0.909], 0),
+        ([0.83, 0.197, 0.83, 0.779], 1),
+        ([0.277, 0.415, 0.277, 0.357], 0),
+        ([0.683, 0.117, 0.683, 0.455], 1),
+        ([0.421, 0.631, 0.421, 0.015], 1)
+    ]
+
+    X, y = map(np.array, zip(*training_examples))
+
+    h = logistic_regression_with_kernel(X, y, monomial_kernel(10), 0.01, 4000)
+
+    test_examples = [
+        ([0.157, 0.715, 0.787, 0.644], 0),
+        ([0.79, 0.279, 0.761, 0.886], 1),
+        ([0.903, 0.544, 0.138, 0.925], 0),
+        ([0.129, 0.01, 0.493, 0.658], 0),
+        ([0.673, 0.526, 0.672, 0.489], 1),
+        ([0.703, 0.716, 0.088, 0.674], 0),
+        ([0.276, 0.174, 0.69, 0.358], 1),
+        ([0.199, 0.812, 0.825, 0.653], 0),
+        ([0.332, 0.721, 0.148, 0.541], 0),
+        ([0.51, 0.956, 0.023, 0.249], 0)
+    ]
+    print(f"{'x' : ^30}{'prediction' : ^11}{'true' : ^6}")
+    for x, y in test_examples:
+        print(f"{str(x) : ^30}{int(h(x)) : ^11}{y : ^6}")
+    #             x               prediction  true
+    # [0.157, 0.715, 0.787, 0.644]      0       0
+    # [0.79, 0.279, 0.761, 0.886]       1       1
+    # [0.903, 0.544, 0.138, 0.925]      0       0
+    # [0.129, 0.01, 0.493, 0.658]       0       0
+    # [0.673, 0.526, 0.672, 0.489]      1       1
+    # [0.703, 0.716, 0.088, 0.674]      0       0
+    # [0.276, 0.174, 0.69, 0.358]       1       1
+    # [0.199, 0.812, 0.825, 0.653]      0       0
+    # [0.332, 0.721, 0.148, 0.541]      0       0
+    # [0.51, 0.956, 0.023, 0.249]       0       0
+
 test1()
 test2()
 test3()
+test4()
